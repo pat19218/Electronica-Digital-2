68c3e6cef72736e5db30be4e6bdab7a7b77864b0
pat19218 <68085020+pat19218@users.noreply.github.com>
2021-10-25 12:24:44 -0600

    hi worldf

---
+++ HolaMundo/main.c
@@ -0,0 +1,21 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/debug.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "driverlib/rom.h"
+#include "grlib/grlib.h"
+#include "drivers/cfal96x64x16.h"
+
+/**
+ * main.c
+ */
+int main(void)
+{
+	return 0;
+}
