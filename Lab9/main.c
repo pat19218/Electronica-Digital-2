b628994c804ed92422479e0a16565c186263558e
pat19218 <68085020+pat19218@users.noreply.github.com>
2021-11-03 19:39:08 -0600

    post lab

--- Lab9/main.c
+++ Lab9/main.c
@@ -1,20 +1,145 @@
+/*
+ * File:        Lab9.c
+ * Dispositivo: Tiva C TM4123G
+ * Autor:       Cristhofer Patzán
+ * Compilador:  CCS V11
+ *
+ * Programa:    Timers
+ * Hardware:    Tiva C TM4123G
+ * Creación:    29 de octubre de 2021
+ * modificacion:3 de octubre de 2021
+ */
 #include <stdint.h>
 #include <stdbool.h>
-#include "inc/hw_ints.h"
+#include "inc/tm4c123gh6pm.h"
+//#include "inc/hw_ints.h"
 #include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
 #include "driverlib/debug.h"
 #include "driverlib/fpu.h"
 #include "driverlib/gpio.h"
 #include "driverlib/interrupt.h"
 #include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
+#include "driverlib/timer.h"
 #include "driverlib/uart.h"
 #include "driverlib/rom.h"
 #include "grlib/grlib.h"
 //#include "drivers/cfal96x64x16.h"
-/**
- * main.c
- */
-int main(void)
-{
-	return 0;
+
+
+#define XTAL 16000000       //16MHz
+
+//------------------------------Variables Globales----------------------------
+uint32_t i = 0;
+uint32_t ui32Period;
+
+//------------------------------Prototipos de Funciones-----------------------
+void delay(uint32_t msec);
+void delay1ms(void);
+void Timer0IntHandler(void);
+
+//------------------------------Función Principal-----------------------------
+
+int main(void){
+    // Configuración del oscilador externo, usando PLL y teniendo una frecuencia de 40MHz
+    SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+
+    // Se asigna reloj a puerto F
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+    // Se establecen como Entradas pin 4 del puerto F como pull-up
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4 , GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+
+    // Se establecen como salidas los pines 1, 2 y 3 del puerto F
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+
+    // Se habilita el reloj para el temporizador
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+
+
+    // Configuración del Timer 0 como temporizador períodico
+    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
+
+    // Se calcula el período para el temporizador (1 seg)
+    ui32Period = (SysCtlClockGet()) / 2;
+    // Establecer el periodo del temporizador
+    TimerLoadSet(TIMER0_BASE, TIMER_A, ui32Period - 1);
+
+    // Se habilita la interrupción por el TIMER0A
+    IntEnable(INT_TIMER0A);
+    // Se establece que exista la interrupción por Timeout
+    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+    // Se habilitan las interrupciones Globales
+    IntMasterEnable();
+    // Se habilita el Timer
+    TimerEnable(TIMER0_BASE, TIMER_A);
+
+    //-------------------------------Loop Principal------------------------------
+	while(1){
+
+	    uint8_t valor = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4);
+        if (valor == 0){
+            TimerDisable(TIMER0_BASE, TIMER_A);
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0);
+	        delay(1000);
+	        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0x08);
+	        delay(1500);
+	        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0);
+	        delay(1000);
+	        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0x08);
+	        delay(1000);
+	        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0);
+	        delay(1000);
+	        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0x08);
+	        delay(1000);
+	        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0);
+	        delay(1000);
+	        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0x0A);
+	        delay(1500);
+	        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0);
+	        delay(1000);
+	        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0x02);
+	        delay(1500);
+	        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, 0);
+	        TimerEnable(TIMER0_BASE, TIMER_A);
+	    }
+
+	}
+
+    return 0;
 }
+
+//--------------------------------------Funciones--------------------------------
+
+void delay1ms(void){            //funcion para hacer delay de 1 milisegundo
+    SysTickDisable();
+    SysTickPeriodSet(16000);
+    SysTickEnable();
+    while ((NVIC_ST_CTRL_R & NVIC_ST_CTRL_COUNT) == 0); //Pg. 138
+}
+
+void delay(uint32_t msec){      //funcion para delay en milisegundos
+    for (i = 0; i < msec; i++){
+        delay1ms();
+    }
+}
+
+//**************************************
+// Handler de la interrupción del TIMER 0 - Recordar modificar el archivo tm4c123ght6pm_startup_css.c
+//**************************************
+void Timer0IntHandler(void){
+    // Clear the timer interrupt
+    TimerIntClear (TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+    // Read the current state of the GPIO pin and
+    // write back the opposite state
+    if (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_2))
+    {
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+    }
+    else
+    {
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 4);
+    }
+}
+
